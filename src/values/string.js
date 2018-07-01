5eaa385e87e70e4f8f1fe0cc70be8d7cdf17bb1f
Jamie Hale <jamiesonhale@gmail.com>
2018-06-30 23:34:47 -0400

    Ramda-ethize string values

--- src/values/string.js
+++ src/values/string.js
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Tablescript.js. If not, see <http://www.gnu.org/licenses/>.
 
+import R from 'ramda';
 import { createValue } from './default';
 import { valueTypes, isString } from './types';
 import { createBooleanValue } from './boolean';
@@ -27,7 +28,7 @@ const nativeEquals = value => (context, other) => isString(other) && value === o
 
 const asString = asNativeString => context => createStringValue(asNativeString(context));
 const asBoolean = asNativeBoolean => context => createBooleanValue(asNativeBoolean(context));
-const equals = nativeEquals => (context, otherValue) => createBooleanValue(nativeEquals(context, otherValue));
+const equals = nativeEquals => (context, other) => createBooleanValue(nativeEquals(context, other));
 
 const getElement = value => (context, index) => {
   let indexValue = index.asNativeNumber(context);
@@ -40,35 +41,28 @@ const getElement = value => (context, index) => {
   return createStringValue(value[indexValue]);
 };
 
-const add = asNativeString => (context, otherValue) => {
-  return createStringValue(asNativeString(context) + otherValue.asNativeString(context));
+const add = asNativeString => (context, other) => createStringValue(asNativeString(context) + other.asNativeString(context));
+const multiplyBy = asNativeString => (context, other) => createStringValue(asNativeString(context).repeat(other.asNativeNumber(context)));
+
+const methods = {
+  asNativeString,
+  asNativeBoolean,
+  nativeEquals,
+  asString: R.pipe(asNativeString, asString),
+  asBoolean: R.pipe(asNativeBoolean, asBoolean),
+  equals: R.pipe(nativeEquals, equals),
+  getElement,
+  add: R.pipe(asNativeString, add),
+  multiplyBy: R.pipe(asNativeString, multiplyBy),
 };
 
-const multiplyBy = asNativeString => (context, otherValue) => {
-  return createStringValue(asNativeString().repeat(otherValue.asNativeNumber()));
-};
+const stringMethods = value => Object.keys(methods).reduce((acc, m) => ({ ...acc, [m]: methods[m](value) }), {});
 
-export const createStringValueWithProperties = (value, properties) => {
-  return createValue(
-    valueTypes.STRING,
-    asNativeString(value),
-    properties,
-    {
-      // native values
-      asNativeString: asNativeString(value),
-      asNativeBoolean: asNativeBoolean(value),
-      nativeEquals: nativeEquals(value),
-      // wrapped values
-      asString: asString(asNativeString(value)),
-      asBoolean: asBoolean(asNativeBoolean(value)),
-      equals: equals(nativeEquals(value)),
-      // elements and properties
-      getElement: getElement(value),
-      // operators
-      add: add(asNativeString),
-      multiplyBy: multiplyBy(asNativeString),
-    },
-  );
-};
+export const createCustomStringValue = (value, properties, methods) => createValue(
+  valueTypes.STRING,
+  asNativeString(value),
+  properties,
+  methods
+);
 
-export const createStringValue = value => createStringValueWithProperties(value, stringProperties(value));
+export const createStringValue = value => createCustomStringValue(value, stringProperties(value), stringMethods(value));
