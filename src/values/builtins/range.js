1a1b03d6e784cc7fad181405f0f7765946e71bde
Jamie Hale <jamiesonhale@gmail.com>
2018-07-03 08:20:08 -0400

    Remove unused import

--- src/values/builtins/range.js
+++ src/values/builtins/range.js
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Tablescript.js. If not, see <http://www.gnu.org/licenses/>.
 
-import { createBuiltInFunctionValue } from '../default';
 import { throwRuntimeError } from '../../error';
 import { createNumericValue } from '../numeric';
 import { createArrayValue } from '../array';
